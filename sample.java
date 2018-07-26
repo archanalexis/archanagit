49b096bec78a299d44cf7fc065145e85381d4c42
archanalexis <archanalexis@gmail.com>
2018-07-26 16:34:28 +0000

    dw

---
+++ sample.java
@@ -0,0 +1,15 @@
+class demo
+{
+  public static void main()
+  {
+    system.output.print("Java");
+  }
+  public static void some()
+  {
+    system.output.print("some");
+  }
+  public static void hello()
+  {
+    system.output.print("hello");
+  }
+}
